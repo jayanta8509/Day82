f5c86cf6e7b291078d05be45fb699c1017f0107f
JayantaRoy <8953307-JayantaRoy@users.noreply.replit.com>
2023-04-10 18:51:23 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> t;
+    string x, y;
+    for (int i = 0; i < n; i++) {
+      cin >> x;
+      if (x[i] == '0') {
+        y += '1';
+      } else {
+        x += '1';
+      }
+    }
+    cout << y << endl;
+  }
+}
